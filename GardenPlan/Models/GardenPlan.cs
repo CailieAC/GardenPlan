e1d3d0a76d035e301b141b28c7bbac11093d837a
CailieAC <cailiecarlile@gmail.com>
2019-08-12 21:14:43 -0500

    Add Plant and Plan Models & Controllers

---
+++ GardenPlan/Models/GardenPlan.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GardenPlan.Models
+{
+    public class GardenPlan
+    {
+        public int Id { get; set; }
+        public List<Plant> Plants { get; set; }
+    }
+}
---
+++ GardenPlan/Models/Plant.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GardenPlan.Models
+{
+    public class Plant
+    {
+        public int Id { get; set; }
+        public string PlantName { get; set; }
+        //change to enum later
+        public string PlantCategory { get; set; }
+        public DateTime PlantDate { get; set; }
+        public DateTime HarvestDate { get; set; }
+        public string Water { get; set; }
+        //change to enum later
+        public string Sun { get; set; }
+        public string Spacing { get; set; }
+    }
+}
