055724132a1209a22748dbc02cc5ad2df3c67b01
CailieAC <cailiecarlile@gmail.com>
2019-08-12 21:51:20 -0500

    Add Duration and Category Enums

---
+++ GardenPlan/Models/Duration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GardenPlan.Models
+{
+    public enum Duration
+    {
+        Perennial,
+        Biennial,
+        Annual,
+        Unknown
+    };
+}
--- GardenPlan/Models/Plant.cs
+++ GardenPlan/Models/Plant.cs
@@ -10,12 +10,13 @@ namespace GardenPlan.Models
         public int Id { get; set; }
         public string PlantName { get; set; }
         //change to enum later
-        public string PlantCategory { get; set; }
+        public PlantCategory PlantCategory { get; set; }
         public DateTime PlantDate { get; set; }
         public DateTime HarvestDate { get; set; }
         public string Water { get; set; }
         //change to enum later
         public string Sun { get; set; }
         public string Spacing { get; set; }
+        public Duration Duration { get; set; }
     }
 }
